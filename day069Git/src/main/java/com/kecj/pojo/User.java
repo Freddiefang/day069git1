4ff99e3989260418c4d4732879df5d538e8fca5c
Freddiefang <fh18371315830@163.com>
2019-08-10 19:38:42 +0800

    updateUser

--- day069Git/src/main/java/com/kecj/pojo/User.java
+++ day069Git/src/main/java/com/kecj/pojo/User.java
@@ -4,6 +4,15 @@ public class User {
 
     private String name;
     private String role;
+    private Integer age;
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 
     public String getName() {
         return name;
